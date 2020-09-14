89e37b0da956d28a94e01ddd950c86e8e88deeae
Facundo Gauna <fgauna12@gmail.com>
2020-09-14 13:31:17 -0700

    wip

--- check-rancher-permissions.sh
+++ check-rancher-permissions.sh
@@ -8,7 +8,7 @@ ACCESS_TOKEN=$(echo "$2")
 
 # query rancher for projects
 # rancher project ls | tail -n +2 | while read -r project
-curl -sH "Authorization: Bearer $ACCESS_TOKEN" "$RANCHER_URL/projects" | jq -r '.data[] | [.id,.name] | @csv' | while read -r project
+curl -sH "Authorization: Bearer $ACCESS_TOKEN" "$RANCHER_URL/projects" | jq -r '.data[] | [.id,.name] | @csv' | tr -d '"' | while read -r project
 do
     PROJECT_ID=$(echo "${project}" | awk -v FS="," '{print $1}')
     PROJECT_NAME=$(echo "${project}" | awk -v FS="," '{print $2}')
@@ -16,11 +16,12 @@ do
 
     # query rancher for members of a project
     # no way to format output from CLI :-(
-    rancher project list-members --project-id "${PROJECT_ID}" | tail -n +2 | sed -E 's/(\s\s+)/,/g' | while read -r member
+    # rancher project list-members --project-id "${PROJECT_ID}" | tail -n +2 | sed -E 's/(\s\s+)/,/g' | while read -r member
+    curl -sH "Authorization: Bearer $ACCESS_TOKEN" "$RANCHER_URL/projectroletemplatebindings?projectId=$PROJECT_ID" | jq -r ".data[] | [.userId, .roleTemplateId] | @csv" | tr -d '"' | while read -r member
     do 
         PROJECT_MEMBER_ID=$(echo "${member}" | awk -F "," '{print $1}')
-        PROJECT_USER=$(echo "${member}" | awk -F "," '{print $2}')
-        PROJECT_ROLE=$(echo "${member}" | awk -F "," '{print $3}')
+        PROJECT_ROLE=$(echo "${member}" | awk -F "," '{print $2}')
+        PROJECT_USER=$(echo "todo")        
 
         echo "$PROJECT_NAME, $PROJECT_MEMBER_ID, $PROJECT_USER, $PROJECT_ROLE" >> $CSV_FILE_NAME
     done 
