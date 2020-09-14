27caeb40f31c84380007a2ab58b5fb82ec4f3631
Facundo Gauna <fgauna12@gmail.com>
2020-09-14 14:53:33 -0700

    adding run.sh to gitignore

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-source check-rancher-permissions.sh "[RANCHER URL]" "[YOUR TOKEN HERE]"
+source check-rancher-permissions.sh "https://rancher.boxops.boxboat.io/v3" "token-jq65g:5qc2xlvzd894vlzdk9bzm2klhrlftc864qsqwbpqbtkczrkhngqdcs"
