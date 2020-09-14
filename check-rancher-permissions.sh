f765dd179bd23f26acbd250e758832f60314720c
Facundo Gauna <fgauna12@gmail.com>
2020-09-14 13:22:46 -0700

    wip getting project id and project name

--- check-rancher-permissions.sh
+++ check-rancher-permissions.sh
@@ -3,12 +3,15 @@
 # Create CSV file
 CSV_FILE_NAME="project-users.csv"
 echo "PROJECT, BINDING-ID, USER, ROLE" > $CSV_FILE_NAME
+RANCHER_URL=$(echo "$1")
+ACCESS_TOKEN=$(echo "$2")
 
 # query rancher for projects
-rancher project ls | tail -n +2 | while read -r project
+# rancher project ls | tail -n +2 | while read -r project
+curl -sH "Authorization: Bearer $ACCESS_TOKEN" "$RANCHER_URL/projects" | jq -r '.data[] | [.id,.name] | @csv' | while read -r project
 do
-    PROJECT_ID=$(echo "${project}" | awk '{print $1}')
-    PROJECT_NAME=$(echo "${project}" | awk '{print $2}')
+    PROJECT_ID=$(echo "${project}" | awk -v FS="," '{print $1}')
+    PROJECT_NAME=$(echo "${project}" | awk -v FS="," '{print $2}')
     echo "Examining project $PROJECT_NAME"
 
     # query rancher for members of a project
