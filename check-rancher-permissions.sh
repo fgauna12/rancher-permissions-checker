70f95e8b119e750e87d9fbaf3939a52e60045309
Facundo Gauna <fgauna12@gmail.com>
2020-09-15 14:17:11 -0700

    Added the cluster name to the CSV

--- check-rancher-permissions.sh
+++ check-rancher-permissions.sh
@@ -2,16 +2,19 @@
 
 # Create CSV file
 CSV_FILE_NAME="project-users.csv"
-echo "PROJECT, BINDING-ID, USER, ROLE" > $CSV_FILE_NAME
+echo "CLUSTER, PROJECT, BINDING-ID, USER, ROLE" > $CSV_FILE_NAME
 RANCHER_URL=$(echo "$1")
 ACCESS_TOKEN=$(echo "$2")
 
 # query rancher for projects
-curl -skH "Authorization: Bearer $ACCESS_TOKEN" "$RANCHER_URL/projects" | jq -r '.data[] | [.id,.name] | @csv' | tr -d '"' | while read -r project
+curl -skH "Authorization: Bearer $ACCESS_TOKEN" "$RANCHER_URL/projects" | jq -r '.data[] | [.id,.name,.clusterId] | @csv' | tr -d '"' | while read -r project
 do
     PROJECT_ID=$(echo "${project}" | awk -v FS="," '{print $1}')
     PROJECT_NAME=$(echo "${project}" | awk -v FS="," '{print $2}')
-    echo "Examining project $PROJECT_NAME"
+    CLUSTER_ID=$(echo "${project}" | awk -v FS="," '{print $3}')
+    echo "Examining project $PROJECT_NAME on Cluster:$CLUSTER_ID"
+
+    CLUSTER_NAME=$(curl -skH "Authorization: Bearer $ACCESS_TOKEN" "$RANCHER_URL/clusters/$CLUSTER_ID" | jq -r '.name')
 
     # query rancher for members of a project
     curl -skH "Authorization: Bearer $ACCESS_TOKEN" "$RANCHER_URL/projectroletemplatebindings?projectId=$PROJECT_ID" | jq -r ".data[] | [.userId, .roleTemplateId] | @csv" | tr -d '"' | while read -r member
@@ -24,7 +27,7 @@ do
             PROJECT_USER='(unknown)'
         fi        
 
-        echo "$PROJECT_NAME, $PROJECT_MEMBER_ID, $PROJECT_USER, $PROJECT_ROLE" >> $CSV_FILE_NAME
+        echo "$CLUSTER_NAME, $PROJECT_NAME, $PROJECT_MEMBER_ID, $PROJECT_USER, $PROJECT_ROLE" >> $CSV_FILE_NAME
     done 
 done
 
